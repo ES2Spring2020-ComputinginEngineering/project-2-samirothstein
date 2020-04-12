71c68d2af70df6ea376f786e5e0a6a25e96cdeff
samirothstein <60266114+samirothstein@users.noreply.github.com>
2020-04-11 21:34:40 -0400

    with comments

--- KMeansClustering_functions.py
+++ KMeansClustering_functions.py
@@ -24,28 +24,81 @@ def normalizeData(glucose, hemoglobin, classification):
 
 
 
-def centroid(k):
+def createCentroid(k):
     #this function is creating random centroids, k
     #2 comes from the amount of features we have
-    array = np.random.rand(k,2)
-    return array
+    centroid = np.random.rand(k,2)
+    return centroid
     
 
-
-def assign_centroid(k,centroids,glucose,hemoglobin):
+   
+def assign_centroid(k,centroid,glucose,hemoglobin):
+    #Takes 4 arguments; k,centroids,glucose,hemoglobin
+    #returns distance_array
     #creates an array with the same amount of rows as centroids
     #the # of columns is equal to amount of data points for hemoglobin
-    distance_array = np.zeros((k,len(hemoglobin)))
+   
+    distance_array = np.zeros((len(hemoglobin),k))
+    assignment = np.zeros(len(hemoglobin))
     for i in range(k):
-        distance = np.sqrt((hemoglobin-array[i,0])**2 + (glucose-array[i,1])**2)
+        distance = np.sqrt((hemoglobin-centroid[i,0])**2 + (glucose-centroid[i,1])**2)
         #this calculates the distance between each data point to each centroid
+        distance_array[:,i] = distance
+   
+    min_index = np.argmin(distance_array, axis=1)
+    assignment[min_index] = min_index
+    return min_index
+
+def update_centroid(centroid,glucose,hemoglobin,k,min_index):
+    #First, I created a random point. Then, I assigned the random point to a centroid.
+    #Created double for loop, that appended the value of glucose/hemo at which it was 
+    #at which hemo that corresponded to certain classification value
+    #Found mean for both Class value of hemo and glucose
+    mean_hemo = 0
+    mean_gluc = 0
+    gluc_classValues = np.array([])
+    hemo_classValues = np.array([])
+#create new point
+#    new_point = np.random.random_sample()
+    assign_centroid(k,centroid,glucose,hemoglobin)
+    #creates an array with k rows, 2 columns
+    for i in range (k):
+        for j in (range(len(min_index))):
+            if min_index[j] == i:
+                gluc_classValues=np.append(gluc_classValues, glucose[j])
+                hemo_classValues=np.append(hemo_classValues,hemoglobin[j])
+        mean_gluc = np.mean(gluc_classValues)
+        mean_hemo = np.mean(hemo_classValues)
+        centroid[i,1] = mean_gluc
+        centroid[i,0] = mean_hemo
+    return (mean_hemo,mean_gluc,centroid)
+
+def iteration(k,trials):
+    glucose,hemoglobin,classification = normalizeData(glucose, hemoglobin, classification)
+    centroid = createCentroid(k)
+    while trials != 0:
+        centroids = assign_centroid(k,centroid,glucose,hemoglobin)
+        centroids = (update_centroid,centroid,glucose,hemoglobin,k,min_index)
+        trials -= 1
+    return (assign_centroid,centroids,glucose,hemoglobin,classification)
 
 
+def graphingKMeans(glucose,hemoglobin,min_index,centroid):
+    plt.figure()
+    for i in range(min_index.max()+1):
+        rcolor = np.random.rand(3,)
+        plt.plot(hemoglobin[min_index==1],glucose[min_index==1],"o",label="Class"+str(i),color=rcolor)
+        plt.plot(centroid[i, 0], centroid[i, 1], "o", label = "Centroid " + str(i), color = rcolor)
+    plt.xlabel("Hemoglobin")
+    plt.ylabel("Glucose")
+    plt.legend()
+    plt.show
 
 
-
-
-
-
-#main script
+glucose,hemoglobin,classification = openckdfile()
 hemoglobin_scaled,glucose_scaled,classification = normalizeData(glucose,hemoglobin,classification)
+centroid = createCentroid(2)
+min_index = assign_centroid(2,centroid,glucose_scaled,hemoglobin_scaled)
+mean_hemo,mean_gluc,centroid = update_centroid(2,centroid,glucose,hemoglobin,min_index)
+graphingKMeans(glucose,hemoglobin,min_index,centroid)
+#main script
--- NearestNeighborClassification.py
+++ NearestNeighborClassification.py
@@ -3,9 +3,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-#import random
 
-#global variables
 
 
 # FUNCTIONS
@@ -16,40 +14,52 @@ def openckdfile():
 
 
 def normalizeData(glucose, hemoglobin, classification):
-    
+#This functiont normalizes the values of the given glucose and hemoglobin values
+#Normalizes it to a 0 or 1 with no units
+#Has 3 parameters: glucose, hemoglobin, classification
+#Returns 3 values: hemoglobin_scaled, glucose_scaled, classification
     g_min = glucose.min()
     g_max = glucose.max()
     h_min = hemoglobin.min()
     h_max = hemoglobin.max()
-   # matplotlib.colors.Normalize(g_min=None, g_max=None, clip=False)
-   #not sure what this does
-    
-    
     hemoglobin_scaled = ((hemoglobin - h_min)/(h_max-h_min))
     glucose_scaled = ((glucose - g_min)/(g_max-g_min))
     return (hemoglobin_scaled, glucose_scaled, classification)
-#print(normalizeData(glucose_scaled,hemoglobin_scared,classification)
+
 
 def graphData(glucose, hemoglobin, classification):
+#This function graphs the data of the normalized hemoglobin and glucose values
+#Has 3 parameters: glucose, hemoglobin, classification
+#This is a void function
    plt.figure()
    plt.plot(hemoglobin_scaled[classification==1],glucose_scaled[classification==1], "k.", label = "Class 1")
    plt.plot(hemoglobin_scaled[classification==0],glucose_scaled[classification==0], "r.", label = "Class 0")
+   plt.title("Normalized Hemoglobin vs Glucose")
    plt.xlabel("Hemoglobin")
    plt.ylabel("Glucose")
    plt.legend()
     
 def testCase():
+#This function creates a test case that we can use in the nearest Neighbor Classifier
+#Takes no parameters
+#Returns newhemoglobin and newglucose, which is the new random data point
     newhemoglobin = np.random.random_sample()
     newglucose = np.random.random_sample()
     return (newhemoglobin, newglucose)
 
 
 def distanceArray(newglucose, newhemoglobin, glucose, hemoglobin):
+#This function finds the value of the distance between the test case and each data point
+#Takes 4 parameters: newglucose, newhemoglobin, glucose, hemoglobin
+#Returns the distance
     distance_array=np.sqrt((hemoglobin - newhemoglobin)**2 + (glucose - newglucose)**2)
     return distance_array
 
 
 def nearestNeighborClassifier(newglucose, newhemoglobin, glucose, hemoglobin, classification):
+#This function takes 5 parameters: newglucose, newhemoglobin, glucose, hemoglobin, classification
+#Uses the distance array from the previous function, and finds the min index of those values
+#Returns the classification of the min_index
     distance = distanceArray(newglucose, newhemoglobin, glucose_scaled, hemoglobin_scaled)
     min_index = np.argmin(distance)
     nearest_class = classification[min_index]
@@ -57,6 +67,9 @@ def nearestNeighborClassifier(newglucose, newhemoglobin, glucose, hemoglobin, cl
 
 
 def graphTestCase(newglucose, newhemoglobin, glucose, hemoglobin, classificiation):
+#This functions graphs the test case along with the normalized data that we already graphed
+#Has 5 parameters: newglucose, newhemoglobin, glucose, hemoglobin, classificiation
+#This is a void function
     plt.figure()
     plt.plot(hemoglobin_scaled[classification==1],glucose_scaled[classification==1], "k.", label = "No CKD")
     plt.plot(hemoglobin_scaled[classification==0],glucose_scaled[classification==0], "r.", label = "Has CKD")
@@ -68,33 +81,27 @@ def graphTestCase(newglucose, newhemoglobin, glucose, hemoglobin, classificiatio
   
 
 def kNearestNeighborClassifier(k, newglucose, newhemoglobin, glucose, hemoglobin, classification):
+#This function classifies the random data point based on the k nearest neighbor method
+#Has 6 parameters: k, newglucose, newhemoglobin, glucose, hemoglobin, classification
+#Returns k_classifications (all the classifications) and final_class (which is the majority of classifications)
    class_0 = 0
    class_1 = 0
    final_class = 0
-    
    distance = distanceArray(newglucose, newhemoglobin, glucose_scaled, hemoglobin_scaled)
    sorted_indices = np.argsort(distance)
    k_indices = sorted_indices[:k]
    k_classifications = classification[k_indices]
-#   k_class = np.median(k_classifications)
    for i in k_classifications:
-    if i == 1:
+       if i == 1:
         class_1 = class_1 + 1
-    if i == 0:
+       if i == 0:
         class_0 = class_0 + 1
    if class_0 > class_1:
        final_class = 0.0
    if class_1 > class_0:
-       final_class = 1.0
-        
-   print(k_classifications,final_class)  
-    
+       final_class = 1.0 
    return k_classifications,final_class
-#    counter = 0
-#    for i in k_classifications:
-#        
-#    return k_classifications
-    
+
 
 # MAIN SCRIPT
 glucose, hemoglobin, classification = openckdfile()
